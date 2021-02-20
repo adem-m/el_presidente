dd492401de5bdd56d5fa99bd52911757b1723cfb
Mana-One <paolo.manaois@yahoo.fr>
2021-02-20 17:28:30 +0100

    printed first event + related choices

--- src/main/java/com/esgi/PlayGameMode.java
+++ src/main/java/com/esgi/PlayGameMode.java
@@ -1,11 +1,16 @@
 package com.esgi;
 
 import com.esgi.builders.StateBuilder;
+import com.esgi.data.Event;
+import com.esgi.data.EventChoice;
 import com.esgi.data.State;
+
+import java.util.List;
 
 public class PlayGameMode extends GameMode
 {
     private State state;
+    private List<EventChoice> currentChoices;
 
     public PlayGameMode( StateBuilder statebuilder )
     {
@@ -16,11 +21,26 @@ public class PlayGameMode extends GameMode
     void init()
     {
         System.out.println( "Game initiated" );
+        Event currentEvent = this.state.getNextEvent();
+        System.out.println( currentEvent.getText() );
+        this.printChoices( currentEvent );
     }
 
     @Override
     void handleInput()
     {
 
+    }
+
+    private void printChoices( Event event )
+    {
+        this.currentChoices = event.getChoices();
+        int index = 0;
+        for( EventChoice choice: this.currentChoices )
+        {
+            System.out.println(
+                    String.format( "%d - %s", ++index, choice.generateLabel(
+                            this.state.getDifficulty() )));
+        }
     }
 }
